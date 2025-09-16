79d5f13902cca2dfc6817f15e93faaccb6ecd756
magalhaestiago <magalhaes_tiago@outlook.com>
2025-09-16 18:05:15 -0300

    fixing repository

--- Documentos/TIago/pdf&ela/sprint-9/new-branch/profilling-docling/src/document_relayout/core/postprocess/remove_key_value_region.py
+++
@@ -1,5 +0,0 @@
-from document_relayout.core.backend.types.box import Box
-
-
-def remove_key_value_region(boxes: list[Box]) -> list[Box]:
-    return [box for box in boxes if box.label != "key-value region"]
